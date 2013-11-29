2179663839e716bdd07019ed9efca0a0a03b5783
Ourai Lin <ourairyu@hotmail.com>
2013-11-29 23:59:40 +0800

    A: core file of Hanger

---
+++ hanger.js
@@ -0,0 +1,12 @@
+/*!
+ * Hanger - Scaffolding of a project
+ *
+ * Copyright 2013, Ourai Lin
+ *
+ * Date: Fri Nov 29 23:57:00 2013
+ */
+;(function( window, $, undefined ) {
+
+"use strict";
+
+})( window, window.jQuery );
