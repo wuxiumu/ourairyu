cb49208f3ff33502b487c94af5cf85ed2f315f7d
Ourai Lin <ourairyu@hotmail.com>
2013-12-02 18:02:35 +0800

    A: return to top

---
+++ plugin/backtotop.js
@@ -0,0 +1,34 @@
+(function() {
+
+$.fn.backtotop = function() {
+  var btn = $(this);
+  var body = document.body;
+  var html = document.documentElement;
+  var tagName;
+
+  btn.click(function() {
+    if ( body.scrollTop ) {
+      tagName = "body";
+    }
+    else if ( html.scrollTop ) {
+      tagName = "html";
+    }
+
+    if ( tagName ) {
+      $(tagName).animate({ scrollTop: 0 });
+    }
+
+    return false;
+  });
+
+  $(window).scroll(function() {
+    if ( (body.scrollTop || html.scrollTop) > 200 ) {
+      btn.fadeOut();
+    }
+    else {
+      btn.fadeIn();
+    }
+  });
+};
+  
+})();
