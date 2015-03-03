5eaaf58b6e4915a1ee8bac5fa06605810db655f7
Ourai Lin <ourairyu@hotmail.com>
2015-03-03 17:14:45 +0800

    添加 tabs with radio buttons 的 demo

--- javascripts/demos/bank_tabs.js
+++ javascripts/demos/bank_tabs.js
@@ -4,4 +4,9 @@
     return $(".tab_bank[data-flag='" + this.id + "']").addClass("show");
   });
 
+  $("body").on("change", ".radioTabs :radio[name='bank']", function() {
+    $(".radioTabs .tab_bank.show").removeClass("show");
+    return $(".radioTabs .tab_bank[data-flag='" + ($(this).val()) + "']").addClass("show");
+  });
+
 }).call(this);
