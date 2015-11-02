4eb6e4d494f271a050dcde5269ddde93490884bb
Ourai Lin <ourairyu@hotmail.com>
2015-11-02 17:28:30 +0800

    加入百度统计

---
+++ _assets/javascripts/global.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?04e5bb9ec25f8a4a2ee9d6060b636583";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
