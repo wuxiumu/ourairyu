6edf0ec13bea942dd7106d01d5c1634515e67420
Ourai Lin <ourairyu@hotmail.com>
2016-02-22 13:50:51 +0800

    Only include statistic code in production environment

--- _assets/javascripts/global.js
+++ _assets/javascripts/global.js
@@ -1,11 +1,2 @@
 //= require ./jquery-1.11.3.min
 //= require ./bootstrap-sprockets.js
-
-// 百度统计
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "//hm.baidu.com/hm.js?04e5bb9ec25f8a4a2ee9d6060b636583";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
